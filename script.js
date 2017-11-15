8c8306c64359137be14cd2a2c922e3fb708e7454
fluoxe7ine <locbkolocb@gmail.com>
2017-11-15 20:43:10 +0200

    some visual updates and arrow funcs

--- script.js
+++ script.js
@@ -1,89 +1,14 @@
 'use strict';
 
+const getRandomArbitary = (min, max) =>
+  Math.floor(Math.random() * (max - min) + min);
+
 let indexOfRandomName = getRandomArbitary(1, 8);
 let indexOfRandomColor = getRandomArbitary(1, 8);
 let indexOfTrueFalse = getRandomArbitary(0, 2);
-let counter = 0;
-let time = 100;
-let shift = 100;
-console.log(indexOfTrueFalse);
 const elem = document.getElementById('change');
-const tick = document.getElementById('counter');
-const line = document.getElementById('timer');
 
-tick.innerHTML = '0';
-generateNewWord();
-
-timer();
-time = 100;
-
-function getRandomArbitary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function generateNewWord() {
-  indexOfTrueFalse = getRandomArbitary(0, 2);
-  indexOfRandomName = getRandomArbitary(1, 8);
-  indexOfRandomColor = getRandomArbitary(1, 8);
-
-  console.log(indexOfTrueFalse);
-  if (indexOfTrueFalse === 1) {
-    while (indexOfRandomName !== indexOfRandomColor) {
-      indexOfRandomName = getRandomArbitary(1, 8);
-      indexOfRandomColor = getRandomArbitary(1, 8);
-    }
-    swapRandomName();
-    swapRandomColor();
-  }
-
-  if (indexOfTrueFalse === 0) {
-    while (indexOfRandomName === indexOfRandomColor) {
-      indexOfRandomName = getRandomArbitary(1, 8);
-      indexOfRandomColor = getRandomArbitary(1, 8);
-    }
-    swapRandomName();
-    swapRandomColor();
-  }
-
-  // indexOfRandomName = getRandomArbitary(1, 8);
-  // indexOfRandomColor = getRandomArbitary(1, 8);
-  // swapRandomName();
-  // swapRandomColor();
-}
-
-function truth() {
-  if (indexOfRandomName === indexOfRandomColor) {
-    counter++;
-    generateNewWord();
-    time = 100;
-    shift = 100;
-  } else {
-    counter = 0;
-    time = 100;
-    shift = 100;
-    generateNewWord();
-  }
-  console.log('counter ' + counter);
-  tick.innerHTML = (counter);
-}
-
-function untruth() {
-  if (indexOfRandomName !== indexOfRandomColor) {
-    counter++;
-    generateNewWord();
-    time = 100;
-    shift = 100;
-  } else {
-    counter = 0;
-    generateNewWord();
-    time = 100;
-    shift = 100;
-  }
-  console.log('counter ' + counter);
-  tick.innerHTML = (counter);
-}
-
-function swapRandomName() {
+const swapRandomName = () => {
   switch (indexOfRandomName) {
     case 1:
       elem.innerHTML = 'Red';
@@ -108,7 +33,7 @@ function swapRandomName() {
       break;
     default:
   }
-}
+};
 
 function swapRandomColor() {
   switch (indexOfRandomColor) {
@@ -137,7 +62,38 @@ function swapRandomColor() {
   }
 }
 
-function timer() {
+const generateNewWord = () => {
+  indexOfTrueFalse = getRandomArbitary(0, 2);
+  indexOfRandomName = getRandomArbitary(1, 8);
+  indexOfRandomColor = getRandomArbitary(1, 8);
+
+  if (indexOfTrueFalse === 1) {
+    while (indexOfRandomName !== indexOfRandomColor) {
+      indexOfRandomName = getRandomArbitary(1, 8);
+      indexOfRandomColor = getRandomArbitary(1, 8);
+    }
+    swapRandomName();
+    swapRandomColor();
+  }
+
+  if (indexOfTrueFalse === 0) {
+    while (indexOfRandomName === indexOfRandomColor) {
+      indexOfRandomName = getRandomArbitary(1, 8);
+      indexOfRandomColor = getRandomArbitary(1, 8);
+    }
+    swapRandomName();
+    swapRandomColor();
+  }
+};
+
+let counter = 0;
+let time = 100;
+let shift = 100;
+
+const tick = document.getElementById('counter');
+const line = document.getElementById('timer');
+
+const timer = () => {
   time = 100;
   const fnc = setInterval(() => {
     time--;
@@ -152,9 +108,72 @@ function timer() {
       return timer(), generateNewWord();
     }
   }, 10);
-}
+};
 
-document.onkeydown = function(event) {
+const leftStyle = document.getElementById('left');
+const rightStyle = document.getElementById('right');
+
+const leftButtonClick = () => {
+  leftStyle.style.backgroundColor = 'black';
+  leftStyle.style.opacity = '0.8';
+  leftStyle.style.color = '#41e25b';
+  setTimeout(() => {
+    leftStyle.style.backgroundColor = 'transparent';
+    leftStyle.style.color = 'white';
+  }, 200);
+};
+
+const rightButtonClick = () => {
+  rightStyle.style.backgroundColor = 'black';
+  rightStyle.style.opacity = '0.8';
+  rightStyle.style.color = '#e22f28';
+  setTimeout(() => {
+    rightStyle.style.backgroundColor = 'transparent';
+    rightStyle.style.color = 'white';
+  }, 200);
+};
+
+const truth = () => {
+  if (indexOfRandomName === indexOfRandomColor) {
+    counter++;
+    generateNewWord();
+    time = 100;
+    shift = 100;
+  } else {
+    counter = 0;
+    time = 100;
+    shift = 100;
+    generateNewWord();
+  }
+  leftButtonClick();
+  console.log('counter ' + counter);
+  tick.innerHTML = (counter);
+};
+
+const untruth = () => {
+  if (indexOfRandomName !== indexOfRandomColor) {
+    counter++;
+    generateNewWord();
+    time = 100;
+    shift = 100;
+  } else {
+    counter = 0;
+    generateNewWord();
+    time = 100;
+    shift = 100;
+  }
+  rightButtonClick();
+  console.log('counter ' + counter);
+  tick.innerHTML = (counter);
+};
+
+tick.innerHTML = '0';
+generateNewWord();
+
+timer();
+time = 100;
+
+document.onkeydown = (event) => {
   if (event.keyCode === 39) untruth();
   if (event.keyCode === 37) truth();
 };
