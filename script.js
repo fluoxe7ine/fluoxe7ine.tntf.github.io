17a844922f09a65e3c90cb4956659cbf55d67183
fluoxe7ine <locbkolocb@gmail.com>
2017-11-21 19:19:03 +0200

    start and end menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let score = 0;
 const getRandomArbitary = (min, max) =>
   Math.floor(Math.random() * (max - min) + min);
 
@@ -102,13 +103,14 @@ const timer = () => {
     if (time <= 0) {
       clearInterval(fnc);
       counter = 0;
-      time = 100;
-      shift = 100;
+      // time = 100;
+      // shift = 100;
       tick.innerHTML = (counter);
-      return timer(), generateNewWord();
+      return generateNewWord(), endGame();
     }
   }, 10);
 };
+
 
 const leftStyle = document.getElementById('left');
 const rightStyle = document.getElementById('right');
@@ -117,9 +119,11 @@ const leftButtonClick = () => {
   leftStyle.style.backgroundColor = 'black';
   leftStyle.style.opacity = '0.8';
   leftStyle.style.color = '#41e25b';
+  leftStyle.style.transform = 'scale(1.1)';
   setTimeout(() => {
     leftStyle.style.backgroundColor = 'transparent';
     leftStyle.style.color = 'white';
+    leftStyle.style.transform = 'none';
   }, 200);
 };
 
@@ -127,15 +131,18 @@ const rightButtonClick = () => {
   rightStyle.style.backgroundColor = 'black';
   rightStyle.style.opacity = '0.8';
   rightStyle.style.color = '#e22f28';
+  rightStyle.style.transform = 'scale(1.1)';
   setTimeout(() => {
     rightStyle.style.backgroundColor = 'transparent';
     rightStyle.style.color = 'white';
+    rightStyle.style.transform = 'none';
   }, 200);
 };
 
 const truth = () => {
   if (indexOfRandomName === indexOfRandomColor) {
     counter++;
+    score = counter;
     generateNewWord();
     time = 100;
     shift = 100;
@@ -144,6 +151,7 @@ const truth = () => {
     time = 100;
     shift = 100;
     generateNewWord();
+    endGame();
   }
   leftButtonClick();
   console.log('counter ' + counter);
@@ -153,6 +161,7 @@ const truth = () => {
 const untruth = () => {
   if (indexOfRandomName !== indexOfRandomColor) {
     counter++;
+    score = counter;
     generateNewWord();
     time = 100;
     shift = 100;
@@ -161,6 +170,7 @@ const untruth = () => {
     generateNewWord();
     time = 100;
     shift = 100;
+    endGame();
   }
   rightButtonClick();
   console.log('counter ' + counter);
@@ -170,10 +180,63 @@ const untruth = () => {
 tick.innerHTML = '0';
 generateNewWord();
 
-timer();
+// timer();
 time = 100;
 
-document.onkeydown = (event) => {
+const leftArrow = document.getElementById('arrow-left');
+const rightArrow = document.getElementById('arrow-right');
+const mouse = document.getElementById('mouse');
+
+const start = () => {
+  timer();
+  document.getElementById('preloader').style.visibility = 'hidden';
+  document.getElementById('blur').style.filter = 'none';
+  document.getElementById('start').style.visibility = 'hidden';
+  document.getElementById('menu-content').style.visibility = 'hidden';
+  mouse.style.transition = 'none';
+  mouse.style.visibility = 'hidden';
+};
+
+
+  document.onkeydown = (e) => {
   if (event.keyCode === 39) untruth();
   if (event.keyCode === 37) truth();
 };
+
+const arrowAnimation = () => {
+  setInterval(() => {
+    leftArrow.style.transform = 'scale(0.9)';
+    leftArrow.style.borderColor = '#41e25b';
+    rightArrow.style.transform = 'none';
+    rightArrow.style.borderColor = 'white';
+    setTimeout(() => {
+      leftArrow.style.transform = 'none';
+      leftArrow.style.borderColor = 'white';
+      rightArrow.style.transform = 'scale(0.9)';
+      rightArrow.style.borderColor = '#e22f28';
+    }, 1000);
+  }, 2000);
+};
+
+const mouseAnimation = () => {
+  setInterval(() => {
+    let shifting = 0;
+    shifting -= 25;
+    mouse.style.left = shifting + 'px';
+    setTimeout(() => {
+      let shifting = 0;
+      shifting += 50;
+      mouse.style.left = shifting + 'px';
+    },1000);
+  }, 2000);
+}
+
+const endGame = () => {
+  document.getElementById('endmenu-content').style.visibility = 'visible';
+  document.getElementById('preloader').style.visibility = 'visible';
+  document.getElementById('score').innerHTML = 'Your score: ' + score;
+}
+
+
+mouseAnimation();
+arrowAnimation();
